f7e942636dff2e43af7ef232c882f010b3f4416d
samaila-bashir <developer.chatto@gmail.com>
2024-09-05 12:08:57 +0100

    configured flowbite

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,15 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Feedback App</h1>;
+  return (
+    <>
+      <div className="md:hidden w-full bg-orange-100 p-4">
+        <h3>FeedWave</h3>
+        <h5>Feedback board</h5>
+      </div>
+      <div className="flex flex-col items-center px-4 md:py-10 lg:flex-row lg:gap-10 lg:justify-center">
+        <div className="w-[100%] md:w-[90%] lg:w-[25%] bg-gray-400">Left</div>
+        <div className="w-[100%] md:w-[90%] lg:w-[60%] bg-gray-600">Right</div>
+      </div>
+    </>
+  );
 };
 export default App;
