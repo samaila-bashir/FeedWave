9d6d0113ee07e870253565950067ad020ae96353
samaila-bashir <developer.chatto@gmail.com>
2024-09-05 16:57:15 +0100

    created sub header section for mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+import hamburger from './assets/shared/mobile/icon-hamburger.svg';
+import { Button } from 'flowbite-react';
+
 const App = () => {
   return (
     <>
-      <div className="md:hidden w-full bg-orange-100 p-4">
-        <h3>FeedWave</h3>
-        <h5>Feedback board</h5>
+      <div className="flex justify-between items-center md:hidden w-full bg-orange-100 p-5 bg-gradient-to-r from-gradient-start via-gradient-mid to-gradient-end">
+        <div>
+          <h3 className="text-xl text-white font-bold">FeedWave</h3>
+          <h5 className="font-medium text-gray-300">Feedback board</h5>
+        </div>
+
+        <div>
+          <img src={hamburger} alt="hamburger" />
+        </div>
       </div>
+
+      <div className="flex justify-between items-center p-5 bg-sub-header-bg">
+        <div className="flex items-center justify-between gap-12">
+          <div className="hidden md:block">
+            <span>icon</span>
+            <p>6 Suggesttions </p>
+          </div>
+
+          <div className="flex justify-between items-center gap-2">
+            <p className="text-[#F2F4FE]">Sort by : </p>
+            <div className="text-white">Upvotes Dropdown</div>
+          </div>
+        </div>
+
+        <Button className="bg-amethyst-purple hover:bg-amethyst-purple-hover">
+          {/* TODO: Fix button hover */}+ Add Feedback
+        </Button>
+      </div>
+
       <div className="flex flex-col items-center px-4 md:py-10 lg:flex-row lg:gap-10 lg:justify-center">
         <div className="w-[100%] md:w-[90%] lg:w-[25%] bg-gray-400">Left</div>
         <div className="w-[100%] md:w-[90%] lg:w-[60%] bg-gray-600">Right</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,26 @@ const flowbite = require('flowbite-react/tailwind');
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'gradient-end': '#E84D70',
+        'gradient-mid': '#A337F6',
+        'gradient-start': '#28A7ED',
+        'amethyst-purple': '#AD1FEA',
+        'amethyst-purple-hover': '#C75AF6',
+        'cornflower-blue': '#4661E6',
+        'ghost-white': '#F2F4FF',
+        'alice-blue': '#F7F8FD',
+        'indigo-ink': '#3A4374',
+        'slate-gray': '#647196',
+        peach: '#F49F85',
+        'sky-blue': '#62BCFA ',
+        'sub-header-bg': '#373F68',
+      },
+      fontFamily: {
+        sans: ['Jost', 'sans-serif'],
+      },
+    },
   },
-  plugins: [flowbite.plugin()],
+  plugins: ['prettier-plugin-tailwindcss', flowbite.plugin()],
 };
